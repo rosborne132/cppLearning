a2d143b2f4c68442476b295a22621eec9e69cd0f
ozborne132@gmail.com <ozborne132@gmail.com>
2022-05-02 08:13:57 -0700

    Create number swapping methods

--- main.cpp
+++ main.cpp
@@ -2,37 +2,25 @@
 
 using namespace std;
 
-void basicASCII() {
-    // These examples are that of the casting operator.
-    // This forces one data type to converge with another.
-    cout << (int)'a' << endl; // 97
-    cout << int('a')<< endl; // 97
-    cout << int('A')<< endl; // 65
+void simpleSwap(int a,  int b) {
+    int temp = a;
+
+    a = b;
+    b = temp;
+
+    cout << "a = " << a << ", b = " << b << endl;
 }
 
-// Method that captures the users input and translates it into ASCII.
-// This method does not include characters 'Enter' or 'Space'.
-void translateLettersFormUser() {
-//    char c1, c2, c3, c4, c5;
-//    cout << "Enter 5 letters: ";
-//    cin >> c1 >> c2 >> c3 >> c4 >> c5;
-//    cout << "ACSII message: " << int(c1) << " " << int(c2)
-//    << " " << int(c3) << " " << int(c4) << " " << int(c5);
-
-    string userInput;
-    cout << "Enter any letters: ";
-    cin >> userInput;
-
-    int inputLength = userInput.length();
-
-    cout << "ACSII message: \n";
-    for (int index = 0; index < inputLength; index++) {
-        cout << int(userInput[index]) << " ";
-    }
+void advancedSwap(int a, int b) {
+    a += b;
+    b = a - b;
+    a -= b;
+    cout << "a = " << a << ", b = " << b << endl;
 }
 
 int main() {
     cout << "Starting Program!\n";
-    basicASCII();
-    translateLettersFormUser();
+
+    simpleSwap(10, 20);
+    advancedSwap(10, 20);
 }
